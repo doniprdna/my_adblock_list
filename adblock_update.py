05d1fb8e991ee25210a5e20bfb4b73cc0359ec2b
doniprdna <118065805+doniprdna@users.noreply.github.com>
2023-01-25 15:10:34 +0700

    Create adblock_update.py

---
+++ adblock_update.py
@@ -0,0 +1,16 @@
+import requests
+
+def get_adblock_list():
+    try:
+        r = requests.get("https://easylist.to/easylist/easylist.txt")
+        adblock_list = r.text.split("\n")
+        adblock_list = ["DOMAIN, " + domain for domain in adblock_list if domain]
+        return adblock_list
+    except Exception as e:
+        print(e)
+        return None
+
+adblock_list = get_adblock_list()
+if adblock_list:
+    with open("adblock_list.txt", "w") as f:
+        f.write("\n".join(adblock_list))
