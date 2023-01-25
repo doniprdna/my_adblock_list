1ec5f8769f030b597fe2a898f8c62789d8703f40
doniprdna <118065805+doniprdna@users.noreply.github.com>
2023-01-25 18:06:06 +0700

    Update adblock_update.py

--- adblock_update.py
+++ adblock_update.py
@@ -2,9 +2,11 @@ import requests
 
 def get_adblock_list():
     try:
-        r = requests.get("https://easylist.to/easylist/easylist.txt")
+        r = requests.get("https://raw.githubusercontent.com/easylist/easylist/master/easylist/easylist_adservers.txt")
         adblock_list = r.text.split("\n")
-        adblock_list = ["DOMAIN, " + domain for domain in adblock_list if domain]
+        adblock_list = [line.replace("||", "").replace("^", "") for line in adblock_list if not line.startswith('!')]
+        adblock_list = ["  - DOMAIN, " + domain for domain in adblock_list if domain]
+        adblock_list.insert(0, "payload:")
         return adblock_list
     except Exception as e:
         print(e)
@@ -12,5 +14,5 @@ def get_adblock_list():
 
 adblock_list = get_adblock_list()
 if adblock_list:
-    with open("adblock_list.yaml", "w", encoding='utf-8') as f:
+    with open("easylist_adservers.yaml", "w", encoding='utf-8') as f:
         f.write("\n".join(adblock_list))
